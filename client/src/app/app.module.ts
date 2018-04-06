69c10c18b653ba4340d889de6870c646810d19b1
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-04-06 16:47:08 +0200

    Removed amexio packages and fix styles.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,22 @@ import { HttpClientModule } from '@angular/common/http';
 // External libraries
 import { ToastModule, ToastOptions } from 'ng2-toastr/ng2-toastr'; // Toaster library used to messaging
 import { ToastrCustomOptions } from './messages/toastrCustomOptions';
-import { AmexioWidgetModule } from 'amexio-ng-extensions'; // Amexio API components library
 import { AgGridModule } from 'ag-grid-angular/main'; // AgGrid component library
 
-import { AppComponent } from './app.component';
+// App modules
+import { AppRoutingModule } from './/app-routing.module';
+import { AuthGuard } from './auth/auth.guard';
 
+// Services
+import { UserService } from './services/user/user.service';
+import { AccountService } from './services/account/account.service';
+import { HeroService } from './services/hero/hero.service';
+import { MessageService } from './services/message/message.service';
+import { GlobalsService } from './globals/globals.service';
+
+// App components
+import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
 import { AccountsComponent } from './components/main/account/accounts/accounts.component';
 import { AccountDetailComponent } from './components/main/account/account-detail/account-detail.component';
 import { HeroesComponent } from './heroes/heroes/heroes.component';
@@ -20,16 +31,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { LoginComponent } from './login/login/login.component';
 import { RegisterComponent } from './login/register/register.component';
-
-import { UserService } from './services/user/user.service';
-import { AccountService } from './services/account/account.service';
-import { HeroService } from './services/hero/hero.service';
-import { MessageService } from './services/message/message.service';
-
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { GlobalsService } from './globals/globals.service';
-import { AuthGuard } from './auth/auth.guard';
 import { CostCentresComponent } from './components/main/costcentres/costcentres/costcentres.component';
 
 
@@ -55,7 +56,6 @@ import { CostCentresComponent } from './components/main/costcentres/costcentres/
     AppRoutingModule,
     HttpClientModule,
     ToastModule.forRoot(),
-    AmexioWidgetModule,
     AgGridModule.withComponents([])
   ],
 
