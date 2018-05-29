41948e307e1574ed1c53d1dcec2a704b49665850
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-05-29 21:58:18 +0200

    Added list of accounts in dashboard.

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,23 +1,72 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-
 import { GlobalsService } from '../globals/globals.service';
-
-import { slideInDownAnimation } from '../animations';
+import { GridOptions } from 'ag-grid';
+import { Router } from '@angular/router';
+import { AccountService } from '../services/account/account.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
-  animations: [ slideInDownAnimation ]
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  @HostBinding('@routeAnimation') routeAnimation = true;
-  @HostBinding('style.display') display = 'block';
-  @HostBinding('style.position') position = 'relative';
+  gridOptions: GridOptions;
+  columnDefs: any[];
+  rowData: any[];
+  frameworkComponents: any;
 
-  constructor(protected globals: GlobalsService) { }
+  constructor(
+    private router: Router,
+    protected globals: GlobalsService,
+    private accountService: AccountService ) {
+      const me = this;
 
-  ngOnInit() {
+      me.getAccounts();
+
+      me.gridOptions = <GridOptions>{
+        rowSelection: 'single',
+        enableColResize: true,
+        enableSorting: true,
+        enableFilter: true,
+        floatingFilter: true,
+        columnDefs: [
+          { headerName: 'Descripción', field: 'description', type: 'textColumn', width: 120 },
+          { headerName: 'Cantidad', field: 'amount', type: ['numericColumn', 'numberColumn'] }
+        ],
+        columnTypes: {
+          numberColumn: {
+            width: 50,
+            valueFormatter: me.currencyFormatter,
+            filter: 'agNumberColumnFilter'
+          },
+          textColumn: {
+            width: 50,
+            filter: 'agTextColumnFilter'
+          }
+        }
+      };
+    }
+
+    onGridReady(params) {
+      params.api.sizeColumnsToFit();
+    }
+
+  // Private Methods
+  private getAccounts(): void {
+    const me = this;
+
+    me.accountService.getAccounts(me.globals.userNameLogged)
+      .subscribe(accounts => {
+        me.rowData = accounts;
+      });
+  }
+
+  currencyFormatter(params) {
+    const me = this,
+          currencyPipe = new CurrencyPipe(navigator.language);
+
+    return currencyPipe.transform(params.value, 'EUR', 'symbol');
   }
 }
