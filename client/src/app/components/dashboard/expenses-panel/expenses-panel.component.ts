8a819ec14fa5171a769c2238cd0982dde200efb6
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-06-06 20:03:33 +0200

    Added logic to get all transactions on expenses deashboard panel.

--- client/src/app/components/dashboard/expenses-panel/expenses-panel.component.ts
+++ client/src/app/components/dashboard/expenses-panel/expenses-panel.component.ts
@@ -3,7 +3,8 @@ import { GridOptions } from 'ag-grid';
 import { Router } from '@angular/router';
 import { GlobalsService } from '../../../globals/globals.service';
 import { AccountService } from '../../../services/account/account.service';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { TransactionService } from '../../../services/transaction/transaction.service';
 
 @Component({
   selector: 'app-expenses-panel',
@@ -22,7 +23,7 @@ export class ExpensesPanelComponent {
   constructor(
     private router: Router,
     protected globals: GlobalsService,
-    private accountService: AccountService ) {
+    private transactionService: TransactionService ) {
       const me = this;
 
       me.panelTitle = 'Gasto Mes: ';
@@ -35,18 +36,28 @@ export class ExpensesPanelComponent {
         enableFilter: true,
         floatingFilter: true,
         columnDefs: [
-          { headerName: 'Descripción', field: 'description', type: 'textColumn', width: 100 },
+          { headerName: 'Fecha', field: 'date', type: 'dateColumn' },
+          { headerName: 'Concepto', field: 'concept.name', type: 'textColumn' },
+          { headerName: 'Centro de gasto', field: 'costCentre.name', type: 'textColumn'},
           { headerName: 'Cantidad', field: 'amount', type: ['numericColumn', 'numberColumn'] }
         ],
         columnTypes: {
           numberColumn: {
-            width: 50,
+            width: 40,
             valueFormatter: me.currencyFormatter,
             filter: 'agNumberColumnFilter'
           },
           textColumn: {
             width: 50,
             filter: 'agTextColumnFilter'
+          },
+          dateColumn: {
+            width: 55,
+            valueFormatter: me.dateFormatter,
+            filter: 'agDateColumnFilter',
+            filterParams: {
+                comparator: me.dateComparator
+            }
           }
         }
       };
@@ -56,19 +67,19 @@ export class ExpensesPanelComponent {
   private getAccounts(): void {
     const me = this;
 
-    me.accountService.getAccounts(me.globals.userNameLogged)
-      .subscribe(accounts => {
-        me.rowData = accounts;
-        me.expensesTotal = me.getTotalExpenses(accounts);
+    me.transactionService.getTransactions(me.globals.userNameLogged)
+      .subscribe(transactions => {
+        me.rowData = transactions;
+        me.expensesTotal = me.getTotalExpenses(transactions);
       });
   }
 
-  private getTotalExpenses(accounts): any {
+  private getTotalExpenses(transactions): any {
     let result = 0;
     const currencyPipe = new CurrencyPipe(navigator.language);
 
-    accounts.forEach(account => {
-      result += account.amount;
+    transactions.forEach(transaction => {
+        result += transaction.amount;
     });
     return currencyPipe.transform(result, 'EUR', 'symbol');
   }
@@ -78,6 +89,26 @@ export class ExpensesPanelComponent {
           currencyPipe = new CurrencyPipe(navigator.language);
 
     return currencyPipe.transform(params.value, 'EUR', 'symbol');
+  }
+
+  dateFormatter(params) {
+    const me = this,
+          datePipe = new DatePipe(navigator.language),
+          format = 'dd/MM/yyyy';
+
+    return datePipe.transform(params.value, format);
+  }
+
+  dateComparator(dateToFilter, cellValue) {
+    const dateToFilterISO = dateToFilter.toISOString();
+
+    if (cellValue < dateToFilterISO) {
+      return -1;
+    } else if (cellValue > dateToFilterISO) {
+          return 1;
+      } else {
+          return 0;
+      }
   }
 
   private onGridReady(params) {
